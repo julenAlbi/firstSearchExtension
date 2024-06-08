c47510346c565421b9e656e3d896454dab5cbcdb
Julen Albizuri <julen.albizuri@nagra.com>
2024-06-08 23:17:30 +0200

    remove logs

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 chrome.omnibox.onInputEntered.addListener((text) => {
-	console.log('Entered text: ', text);
     const query = encodeURIComponent(text);
-	console.log('query :>> ', query);
     const url = `https://www.google.com/search?q=${query}&source=firstSeach`;
-	console.log('url :>> ', url);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         if (tabs.length > 0) {
             chrome.tabs.update(tabs[0].id, { url });
